040d8fb29be2e307dd7ee3463eade8eef6739e0b
Takayuki Sakai <kaky0922@gmail.com>
2017-05-31 19:43:47 +0900

    scaffold spring framework

---
+++ src/main/kotlin/com/thezenos/controller/GreetingController.kt
@@ -0,0 +1,18 @@
+package com.thezenos.controller
+
+import java.util.concurrent.atomic.AtomicLong
+
+import com.thezenos.entity.Greeting
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingController {
+    val counter = AtomicLong()
+
+    @RequestMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        return Greeting(counter.incrementAndGet(), "Hello, $name!")
+    }
+}
---
+++ src/main/kotlin/com/thezenos/entity/Greeting.kt
@@ -0,0 +1,3 @@
+package com.thezenos.entity
+
+data class Greeting(val id: Long, val content: String)
